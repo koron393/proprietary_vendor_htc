58d77acc430e10f5ce31ef47d8fcfe0b10ffc793
scoty755 <scoty755@gmail.com>
2015-07-07 23:13:16 +0900

    b2wlj: add missing drc and eq

--- b2wlj/b2wlj-vendor-blobs.mk
+++ b2wlj/b2wlj-vendor-blobs.mk
@@ -52,7 +52,9 @@ PRODUCT_COPY_FILES += \
         vendor/htc/b2wlj/proprietary/etc/tfa/tfa9895MFG.patch:/system/etc/tfa/tfa9895MFG.patch \
         vendor/htc/b2wlj/proprietary/etc/tfa/tfa9895.patch:/system/etc/tfa/tfa9895.patch \
         vendor/htc/b2wlj/proprietary/etc/tfa/tfa9895.speaker:/system/etc/tfa/tfa9895.speaker \
+        vendor/htc/b2wlj/proprietary/etc/tfa/voice_l.drc:/system/etc/tfa/voice.drc \
         vendor/htc/b2wlj/proprietary/etc/tfa/voice.drc:/system/etc/tfa/voice.drc \
+        vendor/htc/b2wlj/proprietary/etc/tfa/voice_l.eq:/system/etc/tfa/voice.eq \
         vendor/htc/b2wlj/proprietary/etc/tfa/voice.eq:/system/etc/tfa/voice.eq \
         vendor/htc/b2wlj/proprietary/etc/tfa/voice_l.preset:/system/etc/tfa/voice_l.preset \
         vendor/htc/b2wlj/proprietary/etc/tfa/voice.preset:/system/etc/tfa/voice.preset \
